69440eaa7a46e7141d7a98d53dd46e9154601f47
VTFLab <satellite.doll@gmail.com>
2017-08-16 17:07:49 +0900

    fix & add obs-filters/cross-stitching-filter , cross-hatching-filter

--- plugins/obs-filters/cross-stitching-filter.c
+++ plugins/obs-filters/cross-stitching-filter.c
@@ -14,72 +14,85 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #include <obs-module.h>
 
-#define SETTING_OFFSET                  "offset"
-#define SETTING_THRESHOLD_1             "threshold_1"
-#define SETTING_THRESHOLD_2             "threshold_2"
-#define SETTING_THRESHOLD_3             "threshold_3"
-#define SETTING_THRESHOLD_4             "threshold_4"
+#define SETTING_STITCH_GAP            "stitch_gap"
+#define SETTING_NEGATIVE_BIAS         "negative_bias"
+#define SETTING_STITCH_COLOR          "stitch_color"
+#define SETTING_OPACITY               "stitch_opacity"
+#define SETTING_INVERT                "invert"
+#define SETTING_GREYSCALE             "greyscale"
 
-#define OMT                             obs_module_text
-#define TEXT_OFFSET                     OMT("Offset")
-#define TEXT_THRESHOLD_1                OMT("Threshold.1")
-#define TEXT_THRESHOLD_2                OMT("Threshold.2")
-#define TEXT_THRESHOLD_3                OMT("Threshold.3")
-#define TEXT_THRESHOLD_4                OMT("Threshold.4")
+#define OMT                           obs_module_text
+#define TEXT_STITCH_GAP               OMT("Stitch.Gap")
+#define TEXT_NEGATIVE_BIAS            OMT("Negative.Bias")
+#define TEXT_STITCH_COLOR             OMT("Stitch.Color")
+#define TEXT_OPACITY                  OMT("Stitch.Opacity")
+#define TEXT_INVERT                   OMT("Invert.Stitching")
+#define TEXT_GREYSCALE                OMT("Greyscale")
 
 
-struct crosshatching_filter_data {
+struct cross_stitching_filter_data {
 	obs_source_t                   *context;
 
 	gs_effect_t                    *effect;
 
-	gs_eparam_t                    *offset_param;
-	gs_eparam_t                    *threshold_1_param;
-	gs_eparam_t                    *threshold_2_param;
-	gs_eparam_t                    *threshold_3_param;
-	gs_eparam_t                    *threshold_4_param;
+	gs_eparam_t                    *stitch_gap_param;
+	gs_eparam_t                    *negative_bias_param;
+	gs_eparam_t                    *stitch_color_param;
+	gs_eparam_t                    *invert_param;
+	gs_eparam_t                    *greyscale_param;
 
-	uint32_t                        offset;
-	float                           threshold_1;
-	float                           threshold_2;
-	float                           threshold_3;
-	float                           threshold_4;
+	uint8_t                         stitch_gap;
+	uint8_t                         negative_bias;
+	struct vec4                     stitch_color;
+	bool                            invert;
+	bool                            greyscale;
 };
 
 
-/* As the functions' namesake, this provides the user facing name
- * of your Filter. */
-static const char *crosshatching_filter_name(void *unused)
+/*
+ * As the functions' namesake, this provides the user facing name
+ * of your Filter.
+ */
+static const char *cross_stitching_filter_name(void *unused)
 {
 	UNUSED_PARAMETER(unused);
-	return obs_module_text("Crosshatching");
+	return obs_module_text("Cross.Stitching");
 }
 
-/* This function is called (see bottom of this file for more details
+/*
+ * This function is called (see bottom of this file for more details
  * whenever the OBS filter interface changes. So when the user is messing
  * with a slider this function is called to update the internal settings
- * in OBS, and hence the settings being passed to the CPU/GPU. */
-static void crosshatching_filter_update(void *data, obs_data_t *settings)
+ * in OBS, and hence the settings being passed to the CPU/GPU.
+ */
+static void cross_stitching_filter_update(void *data, obs_data_t *settings)
 {
-	struct crosshatching_filter_data *filter = data;
-	filter->offset = (uint32_t)obs_data_get_int(settings,
-			SETTING_OFFSET);
-	filter->threshold_1 = (float)obs_data_get_double(settings,
-			SETTING_THRESHOLD_1);
-	filter->threshold_2 = (float)obs_data_get_double(settings,
-			SETTING_THRESHOLD_2);
-	filter->threshold_3 = (float)obs_data_get_double(settings,
-			SETTING_THRESHOLD_3);
-	filter->threshold_4 = (float)obs_data_get_double(settings,
-			SETTING_THRESHOLD_4);
+	struct cross_stitching_filter_data *filter = data;
+	filter->stitch_gap = (uint8_t)obs_data_get_int(settings,
+			SETTING_STITCH_GAP);
+	filter->negative_bias = (uint8_t)obs_data_get_int(settings,
+			SETTING_NEGATIVE_BIAS);
+	uint32_t stitch_color = (uint32_t)obs_data_get_int(settings,
+			SETTING_STITCH_COLOR);
+	uint32_t stitch_opacity = (uint32_t)obs_data_get_int(settings,
+			SETTING_OPACITY);
+	filter->invert = obs_data_get_bool(settings, SETTING_INVERT);
+	filter->greyscale = obs_data_get_bool(settings, SETTING_GREYSCALE);
+
+	/* Convert Stitch Color from a uint32_t to a vec4 */
+	stitch_color &= 0xFFFFFF;
+	stitch_color |= ((stitch_opacity * 255) / 100) << 24;
+	vec4_from_rgba(&filter->stitch_color, stitch_color);
 }
 
-/* Since this is C we have to be careful when destroying/removing items from
+/*
+ * Since this is C we have to be careful when destroying/removing items from
  * OBS. Jim has added several useful functions to help keep memory leaks to
- * a minimum, and handle the destruction and construction of these filters. */
-static void crosshatching_filter_destroy(void *data)
+ * a minimum, and handle the destruction and construction of these filters.
+ */
+static void cross_stitching_filter_destroy(void *data)
 {
-	struct crosshatching_filter_data *filter = data;
+	struct cross_stitching_filter_data *filter = data;
 
 	if (filter->effect) {
 		obs_enter_graphics();
@@ -96,27 +109,27 @@ static void crosshatching_filter_destroy(void *data)
  * filter, it also calls the render function (farther below) that contains the
  * actual rendering code.
  */
-static void *crosshatching_filter_create(obs_data_t *settings, obs_source_t *context)
+static void *cross_stitching_filter_create(obs_data_t *settings,
+		obs_source_t *context)
 {
 	/*
-	 * Because of limitations of pre-c99 compilers, you can't create an
+	 * Because of limitations of pre-C99 compilers, you can't create an
 	 * array that doesn't have a know size at compile time. The below
 	 * function calculates the size needed and allocates memory to
 	 * handle the source.
 	 */
-	struct crosshatching_filter_data *filter =
-			bzalloc(sizeof(struct crosshatching_filter_data));
+	struct cross_stitching_filter_data *filter =
+			bzalloc(sizeof(struct cross_stitching_filter_data));
 
 	/*
 	 * By default the effect file is stored in the ./data directory that
 	 * your filter resides in.
 	 */
 	#ifdef WIN32
-		char *effect_path = obs_module_file("crosshatching_filter.effect");
+		char *effect_path = obs_module_file("cross_stitching_filter.effect");
 	#else
-		char *effect_path = obs_module_file("crosshatching_filter_gl.effect");
+		char *effect_path = obs_module_file("cross_stitching_filter_gl.effect");
 	#endif
-
 
 	filter->context = context;
 
@@ -126,21 +139,18 @@ static void *crosshatching_filter_create(obs_data_t *settings, obs_source_t *con
 	/* Load the shader on the GPU. */
 	filter->effect = gs_effect_create_from_file(effect_path, NULL);
 
-	/*
-	 * If the filter exists grab the param names and assign them to the
-	 * struct for later recall/use.
-	 */
+	/* If the filter is active pass the parameters to the filter. */
 	if (filter->effect) {
-		filter->offset_param = gs_effect_get_param_by_name(
-				filter->effect, SETTING_OFFSET);
-		filter->threshold_1_param = gs_effect_get_param_by_name(
-				filter->effect, SETTING_THRESHOLD_1);
-		filter->threshold_2_param = gs_effect_get_param_by_name(
-				filter->effect, SETTING_THRESHOLD_2);
-		filter->threshold_3_param = gs_effect_get_param_by_name(
-				filter->effect, SETTING_THRESHOLD_3);
-		filter->threshold_4_param = gs_effect_get_param_by_name(
-				filter->effect, SETTING_THRESHOLD_4);
+		filter->stitch_gap_param = gs_effect_get_param_by_name(
+				filter->effect, SETTING_STITCH_GAP);
+		filter->negative_bias_param = gs_effect_get_param_by_name(
+				filter->effect, SETTING_NEGATIVE_BIAS);
+		filter->stitch_color_param = gs_effect_get_param_by_name(
+				filter->effect, SETTING_STITCH_COLOR);
+		filter->invert_param = gs_effect_get_param_by_name(
+				filter->effect, SETTING_INVERT);
+		filter->greyscale_param = gs_effect_get_param_by_name(
+				filter->effect, SETTING_GREYSCALE);
 	}
 
 	obs_leave_graphics();
@@ -148,7 +158,7 @@ static void *crosshatching_filter_create(obs_data_t *settings, obs_source_t *con
 	bfree(effect_path);
 
 	if (!filter->effect) {
-		crosshatching_filter_destroy(filter);
+		cross_stitching_filter_destroy(filter);
 		return NULL;
 	}
 
@@ -157,30 +167,27 @@ static void *crosshatching_filter_create(obs_data_t *settings, obs_source_t *con
 	 * we could end up with the user controlled sliders and values
 	 * updating, but the visuals not updating to match.
 	 */
-	crosshatching_filter_update(filter, settings);
+	cross_stitching_filter_update(filter, settings);
 	return filter;
 }
 
 /* This is where the actual rendering of the filter takes place. */
-static void crosshatching_filter_render(void *data, gs_effect_t *effect)
+static void cross_stitching_filter_render(void *data, gs_effect_t *effect)
 {
-	struct crosshatching_filter_data *filter = data;
+	struct cross_stitching_filter_data *filter = data;
 
 	if (!obs_source_process_filter_begin(filter->context, GS_RGBA,
-				OBS_ALLOW_DIRECT_RENDERING))
+			OBS_ALLOW_DIRECT_RENDERING))
 		return;
 
 	/* Now pass the interface variables to the .shader file. */
-	gs_effect_set_float(filter->offset_param,
-			(float)filter->offset);
-	gs_effect_set_float(filter->threshold_1_param,
-			filter->threshold_1);
-	gs_effect_set_float(filter->threshold_2_param,
-			filter->threshold_2);
-	gs_effect_set_float(filter->threshold_3_param,
-			filter->threshold_3);
-	gs_effect_set_float(filter->threshold_4_param,
-			filter->threshold_4);
+	gs_effect_set_float(filter->stitch_gap_param,
+			(float)filter->stitch_gap);
+	gs_effect_set_float(filter->negative_bias_param,
+			(float)filter->negative_bias);
+	gs_effect_set_vec4(filter->stitch_color_param, &filter->stitch_color);
+	gs_effect_set_bool(filter->invert_param, filter->invert);
+	gs_effect_set_bool(filter->greyscale_param, filter->greyscale);
 
 	obs_source_process_filter_end(filter->context, filter->effect, 0, 0);
 
@@ -188,25 +195,23 @@ static void crosshatching_filter_render(void *data, gs_effect_t *effect)
 }
 
 /*
- * This function sets the interface. the types (add_*_Slider), the type of
+ * This function sets the interface, the types (add_*_Slider), the type of
  * data collected (int), the internal name, user-facing name, minimum,
  * maximum and step values. While a custom interface can be built, for a
  * simple filter like this it's better to use the supplied functions.
  */
-static obs_properties_t *crosshatching_filter_properties(void *data)
+static obs_properties_t *cross_stitching_filter_properties(void *data)
 {
 	obs_properties_t *props = obs_properties_create();
 
-	obs_properties_add_int_slider(props, SETTING_OFFSET,
-			TEXT_OFFSET, 1, 10, 1);
-	obs_properties_add_float_slider(props, SETTING_THRESHOLD_1,
-			TEXT_THRESHOLD_1, 0.01f, 1.0f, 0.01f);
-	obs_properties_add_float_slider(props, SETTING_THRESHOLD_2,
-			TEXT_THRESHOLD_2, 0.01f, 1.0f, 0.01f);
-	obs_properties_add_float_slider(props, SETTING_THRESHOLD_3,
-			TEXT_THRESHOLD_3, 0.01f, 1.0f, 0.01f);
-	obs_properties_add_float_slider(props, SETTING_THRESHOLD_4,
-			TEXT_THRESHOLD_4, 0.01f, 1.0f, 0.01f);
+	obs_properties_add_int_slider(props, SETTING_STITCH_GAP,
+			TEXT_STITCH_GAP, 1, 15, 1);
+	obs_properties_add_int_slider(props, SETTING_NEGATIVE_BIAS,
+			TEXT_NEGATIVE_BIAS, 1, 10, 1);
+	obs_properties_add_color(props, SETTING_STITCH_COLOR,
+			TEXT_STITCH_COLOR);
+	obs_properties_add_bool(props, SETTING_INVERT, TEXT_INVERT);
+	obs_properties_add_bool(props, SETTING_GREYSCALE, TEXT_GREYSCALE);
 
 	UNUSED_PARAMETER(data);
 	return props;
@@ -218,13 +223,14 @@ static obs_properties_t *crosshatching_filter_properties(void *data)
  * completely optional, as is providing a default for any particular
  * option.
  */
-static void crosshatching_filter_defaults(obs_data_t *settings)
+static void cross_stitching_filter_defaults(obs_data_t *settings)
 {
-	obs_data_set_default_int(settings, SETTING_OFFSET, 5);
-	obs_data_set_default_double(settings, SETTING_THRESHOLD_1, 1.0);
-	obs_data_set_default_double(settings, SETTING_THRESHOLD_2, 0.7);
-	obs_data_set_default_double(settings, SETTING_THRESHOLD_3, 0.5);
-	obs_data_set_default_double(settings, SETTING_THRESHOLD_4, 0.3);
+	obs_data_set_default_int(settings, SETTING_STITCH_GAP, 6);
+	obs_data_set_default_int(settings, SETTING_NEGATIVE_BIAS, 1);
+	obs_data_set_default_int(settings, SETTING_STITCH_COLOR, 0x000000);
+	obs_data_set_default_int(settings, SETTING_OPACITY, 100);
+	obs_data_set_default_bool(settings, SETTING_INVERT, 0);
+	obs_data_set_default_bool(settings, SETTING_GREYSCALE, 0);
 }
 
 /*
@@ -237,15 +243,15 @@ static void crosshatching_filter_defaults(obs_data_t *settings)
  * variable name in it? While not mandatory, it helps a ton for you (and those
  * reading your code) to follow this convention.
  */
-struct obs_source_info crosshatching_filter = {
-	.id                            = "crosshatching_filter",
+struct obs_source_info cross_stitching_filter = {
+	.id                            = "cross_stitching_filter",
 	.type                          = OBS_SOURCE_TYPE_FILTER,
 	.output_flags                  = OBS_SOURCE_VIDEO,
-	.get_name                      = crosshatching_filter_name,
-	.create                        = crosshatching_filter_create,
-	.destroy                       = crosshatching_filter_destroy,
-	.video_render                  = crosshatching_filter_render,
-	.update                        = crosshatching_filter_update,
-	.get_properties                = crosshatching_filter_properties,
-	.get_defaults                  = crosshatching_filter_defaults
+	.get_name                      = cross_stitching_filter_name,
+	.create                        = cross_stitching_filter_create,
+	.destroy                       = cross_stitching_filter_destroy,
+	.video_render                  = cross_stitching_filter_render,
+	.update                        = cross_stitching_filter_update,
+	.get_properties                = cross_stitching_filter_properties,
+	.get_defaults                  = cross_stitching_filter_defaults
 };
