e48d3749d773baf336e65fcd0d8a74a79a03d56d
VTFLAB <satellite.doll@gmail.com>
2018-04-11 17:16:16 +0900

    Update obs-app.cpp

--- UI/obs-app.cpp
+++ UI/obs-app.cpp
@@ -415,6 +415,9 @@ bool OBSApp::InitGlobalConfigDefaults()
 				"CurrentTheme", "Dark");
 	}
 
+	config_set_default_bool(globalConfig, "BasicWindow",
+			"VerticalVolControl", false);
+
 #ifdef _WIN32
 	config_set_default_bool(globalConfig, "Audio", "DisableAudioDucking",
 			true);
